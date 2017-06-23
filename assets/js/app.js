ded158c95264d5d75629f62ba5218b5d1b15f0c7
Flowmar <omar.imam@kineticvortex.com>
2017-06-23 14:46:16 -0500

    Finish page structure

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,22 @@
-// Declare variables
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBF6rlxRpM2fcb7YkI6TwXreHmVpuXf21E",
+    authDomain: "awesomeness-cc93e.firebaseapp.com",
+    databaseURL: "https://awesomeness-cc93e.firebaseio.com",
+    projectId: "awesomeness-cc93e",
+    storageBucket: "awesomeness-cc93e.appspot.com",
+    messagingSenderId: "1044603233814"
+  };
+  firebase.initializeApp(config);
 
-var playerOne;
+// Declare variables
+var nameinput = "";
+
+// Constructors
+function Player(name, wins, losses) = {
+    this.name = name;
+    this.wins = wins;
+    this.losses = losses;
+};
+
+var playerOne = new Player(nameinput, );
